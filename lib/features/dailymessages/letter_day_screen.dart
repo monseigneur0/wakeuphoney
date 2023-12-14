b8c39a1fd1be4624989139e4175198993994fc70
monseigneur0 <82815356+monseigneur0@users.noreply.github.com>
2023-12-14 23:58:01 +0900

    size of ads

--- lib/features/dailymessages/letter_day_screen.dart
+++ lib/features/dailymessages/letter_day_screen.dart
@@ -219,7 +219,7 @@ class _LetterDayScreenState extends ConsumerState<LetterDayScreen> {
               alignment: Alignment.bottomCenter,
               child: SizedBox(
                 width: MediaQuery.of(context).size.width,
-                height: 250,
+                height: MediaQuery.of(context).size.width > 800 ? 300 : 150,
                 child: AdWidget(ad: _bannerAd!),
               ),
             ),
--- lib/features/profile/profile_screen.dart
+++ lib/features/profile/profile_screen.dart
@@ -212,7 +212,7 @@ class _CoupleProfileScreenState extends ConsumerState<CoupleProfileScreen> {
               alignment: Alignment.bottomCenter,
               child: SizedBox(
                 width: MediaQuery.of(context).size.width,
-                height: 300,
+                height: MediaQuery.of(context).size.width > 800 ? 300 : 150,
                 child: AdWidget(ad: _bannerAd!),
               ),
             ),
