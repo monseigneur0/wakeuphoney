bb89f6b5f59c611e53dea8e73b3cdf6a05420155
monseigneur0 <82815356+monseigneur0@users.noreply.github.com>
2024-02-15 21:18:22 +0900

    Update just_audio_examle.dart

--- lib/features/voice/just_audio_examle.dart
+++ lib/features/voice/just_audio_examle.dart
@@ -21,7 +21,7 @@ class JustAudioExample extends StatefulWidget {
 
 class JustAudioExampleState extends State<JustAudioExample>
     with WidgetsBindingObserver {
-  final _player = AudioPlayer();
+  final _player = const AudioPlayer();
 
   late List<AudioSource> _playlist;
 
@@ -434,3 +434,517 @@ void showSliderDialog({
 }
 
 T? ambiguate<T>(T? value) => value;
+
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'package:record_example/audio_player.dart';
+import 'package:record_example/audio_recorder.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool showPlayer = false;
+  String? audioPath;
+
+  @override
+  void initState() {
+    showPlayer = false;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: showPlayer
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: AudioPlayer(
+                    source: audioPath!,
+                    onDelete: () {
+                      setState(() => showPlayer = false);
+                    },
+                  ),
+                )
+              : Recorder(
+                  onStop: (path) {
+                    if (kDebugMode) print('Recorded file path: $path');
+                    setState(() {
+                      audioPath = path;
+                      showPlayer = true;
+                    });
+                  },
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:record/record.dart';
+import 'package:wave_blob/wave_blob.dart';
+
+import 'platform/audio_recorder_platform.dart';
+
+class Recorder extends StatefulWidget {
+  final void Function(String path) onStop;
+
+  const Recorder({super.key, required this.onStop});
+
+  @override
+  State<Recorder> createState() => _RecorderState();
+}
+
+class _RecorderState extends State<Recorder> with AudioRecorderMixin {
+  int _recordDuration = 0;
+  Timer? _timer;
+  late final AudioRecorder _audioRecorder;
+  StreamSubscription<RecordState>? _recordSub;
+  RecordState _recordState = RecordState.stop;
+  StreamSubscription<Amplitude>? _amplitudeSub;
+  Amplitude? _amplitude;
+  final double _amplitudeValue = 4250;
+
+  @override
+  void initState() {
+    _audioRecorder = AudioRecorder();
+
+    _recordSub = _audioRecorder.onStateChanged().listen((recordState) {
+      _updateRecordState(recordState);
+    });
+
+    _amplitudeSub = _audioRecorder
+        .onAmplitudeChanged(const Duration(milliseconds: 300))
+        .listen((amp) {
+      setState(() => _amplitude = amp);
+    });
+
+    super.initState();
+  }
+
+  Future<void> _start() async {
+    try {
+      if (await _audioRecorder.hasPermission()) {
+        const encoder = AudioEncoder.aacLc;
+
+        if (!await _isEncoderSupported(encoder)) {
+          return;
+        }
+
+        final devs = await _audioRecorder.listInputDevices();
+        debugPrint(devs.toString());
+
+        const config = RecordConfig(encoder: encoder, numChannels: 1);
+
+        // Record to file
+        await recordFile(_audioRecorder, config);
+
+        // Record to stream
+        // await recordStream(_audioRecorder, config);
+
+        _recordDuration = 0;
+
+        _startTimer();
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Future<void> _stop() async {
+    final path = await _audioRecorder.stop();
+
+    if (path != null) {
+      widget.onStop(path);
+
+      downloadWebData(path);
+    }
+  }
+
+  Future<void> _pause() => _audioRecorder.pause();
+
+  Future<void> _resume() => _audioRecorder.resume();
+
+  void _updateRecordState(RecordState recordState) {
+    setState(() => _recordState = recordState);
+
+    switch (recordState) {
+      case RecordState.pause:
+        _timer?.cancel();
+        break;
+      case RecordState.record:
+        _startTimer();
+        break;
+      case RecordState.stop:
+        _timer?.cancel();
+        _recordDuration = 0;
+        break;
+    }
+  }
+
+  Future<bool> _isEncoderSupported(AudioEncoder encoder) async {
+    final isSupported = await _audioRecorder.isEncoderSupported(
+      encoder,
+    );
+
+    if (!isSupported) {
+      debugPrint('${encoder.name} is not supported on this platform.');
+      debugPrint('Supported encoders are:');
+
+      for (final e in AudioEncoder.values) {
+        if (await _audioRecorder.isEncoderSupported(e)) {
+          debugPrint('- ${encoder.name}');
+        }
+      }
+    }
+
+    return isSupported;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _buildRecordStopControl(),
+                const SizedBox(width: 20),
+                _buildPauseResumeControl(),
+                const SizedBox(width: 20),
+                _buildText(),
+              ],
+            ),
+            if (_amplitude != null) ...[
+              const SizedBox(height: 40),
+              Text('Current: ${_amplitude?.current ?? 0.0}'),
+              Text('Current: ${-(_amplitude?.current ?? 0.0) * 10}'),
+              SizedBox(
+                width: MediaQuery.sizeOf(context).width * 0.4,
+                height: MediaQuery.sizeOf(context).width * 0.4,
+                child: WaveBlob(
+                  amplitude: -(_amplitude?.current ?? 0.0) * 10,
+                  child: const Icon(
+                    Icons.mic,
+                    color: Colors.white,
+                    size: 30.0,
+                  ),
+                ),
+              ),
+              Text('Max: ${_amplitude?.max ?? 0.0}'),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _recordSub?.cancel();
+    _amplitudeSub?.cancel();
+    _audioRecorder.dispose();
+    super.dispose();
+  }
+
+  Widget _buildRecordStopControl() {
+    late Icon icon;
+    late Color color;
+
+    if (_recordState != RecordState.stop) {
+      icon = const Icon(Icons.stop, color: Colors.red, size: 30);
+      color = Colors.red.withOpacity(0.1);
+    } else {
+      final theme = Theme.of(context);
+      icon = Icon(Icons.mic, color: theme.primaryColor, size: 30);
+      color = theme.primaryColor.withOpacity(0.1);
+    }
+
+    return ClipOval(
+      child: Material(
+        color: color,
+        child: InkWell(
+          child: SizedBox(
+            width: MediaQuery.sizeOf(context).width * 0.2,
+            height: MediaQuery.sizeOf(context).width * 0.2,
+            child: WaveBlob(
+              amplitude: -(_amplitude?.current ?? 0.0) * 10,
+              child: const Icon(
+                Icons.mic,
+                color: Colors.white,
+                size: 30.0,
+              ),
+            ),
+          ),
+          onTap: () {
+            (_recordState != RecordState.stop) ? _stop() : _start();
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPauseResumeControl() {
+    if (_recordState == RecordState.stop) {
+      return const SizedBox.shrink();
+    }
+
+    late Icon icon;
+    late Color color;
+
+    if (_recordState == RecordState.record) {
+      icon = const Icon(Icons.pause, color: Colors.red, size: 30);
+      color = Colors.red.withOpacity(0.1);
+    } else {
+      final theme = Theme.of(context);
+      icon = const Icon(Icons.play_arrow, color: Colors.red, size: 30);
+      color = theme.primaryColor.withOpacity(0.1);
+    }
+
+    return ClipOval(
+      child: Material(
+        color: color,
+        child: InkWell(
+          child: SizedBox(width: 56, height: 56, child: icon),
+          onTap: () {
+            (_recordState == RecordState.pause) ? _resume() : _pause();
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildText() {
+    if (_recordState != RecordState.stop) {
+      return _buildTimer();
+    }
+
+    return const Text("Waiting to record");
+  }
+
+  Widget _buildTimer() {
+    final String minutes = _formatNumber(_recordDuration ~/ 60);
+    final String seconds = _formatNumber(_recordDuration % 60);
+
+    return Text(
+      '$minutes : $seconds',
+      style: const TextStyle(color: Colors.red),
+    );
+  }
+
+  String _formatNumber(int number) {
+    String numberStr = number.toString();
+    if (number < 10) {
+      numberStr = '0$numberStr';
+    }
+
+    return numberStr;
+  }
+
+  void _startTimer() {
+    _timer?.cancel();
+
+    _timer = Timer.periodic(const Duration(seconds: 1), (Timer t) {
+      setState(() => _recordDuration++);
+    });
+  }
+}
+
+
+import 'dart:async';
+
+import 'package:audioplayers/audioplayers.dart' as ap;
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class AudioPlayer extends StatefulWidget {
+  /// Path from where to play recorded audio
+  final String source;
+
+  /// Callback when audio file should be removed
+  /// Setting this to null hides the delete button
+  final VoidCallback onDelete;
+
+  const AudioPlayer({
+    super.key,
+    required this.source,
+    required this.onDelete,
+  });
+
+  @override
+  AudioPlayerState createState() => AudioPlayerState();
+}
+
+class AudioPlayerState extends State<AudioPlayer> {
+  static const double _controlSize = 56;
+  static const double _deleteBtnSize = 24;
+
+  final _audioPlayer = ap.AudioPlayer()..setReleaseMode(ReleaseMode.stop);
+  late StreamSubscription<void> _playerStateChangedSubscription;
+  late StreamSubscription<Duration?> _durationChangedSubscription;
+  late StreamSubscription<Duration> _positionChangedSubscription;
+  Duration? _position;
+  Duration? _duration;
+
+  @override
+  void initState() {
+    _playerStateChangedSubscription =
+        _audioPlayer.onPlayerComplete.listen((state) async {
+      await stop();
+    });
+    _positionChangedSubscription = _audioPlayer.onPositionChanged.listen(
+      (position) => setState(() {
+        _position = position;
+      }),
+    );
+    _durationChangedSubscription = _audioPlayer.onDurationChanged.listen(
+      (duration) => setState(() {
+        _duration = duration;
+      }),
+    );
+
+    _audioPlayer.setSource(_source);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _playerStateChangedSubscription.cancel();
+    _positionChangedSubscription.cancel();
+    _durationChangedSubscription.cancel();
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                _buildControl(),
+                _buildSlider(constraints.maxWidth),
+                IconButton(
+                  icon: const Icon(Icons.delete,
+                      color: Color(0xFF73748D), size: _deleteBtnSize),
+                  onPressed: () {
+                    if (_audioPlayer.state == ap.PlayerState.playing) {
+                      stop().then((value) => widget.onDelete());
+                    } else {
+                      widget.onDelete();
+                    }
+                  },
+                ),
+              ],
+            ),
+            Text('${_duration ?? 0.0}'),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildControl() {
+    Icon icon;
+    Color color;
+
+    if (_audioPlayer.state == ap.PlayerState.playing) {
+      icon = const Icon(Icons.pause, color: Colors.red, size: 30);
+      color = Colors.red.withOpacity(0.1);
+    } else {
+      final theme = Theme.of(context);
+      icon = Icon(Icons.play_arrow, color: theme.primaryColor, size: 30);
+      color = theme.primaryColor.withOpacity(0.1);
+    }
+
+    return ClipOval(
+      child: Material(
+        color: color,
+        child: InkWell(
+          child:
+              SizedBox(width: _controlSize, height: _controlSize, child: icon),
+          onTap: () {
+            if (_audioPlayer.state == ap.PlayerState.playing) {
+              pause();
+            } else {
+              play();
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSlider(double widgetWidth) {
+    bool canSetValue = false;
+    final duration = _duration;
+    final position = _position;
+
+    if (duration != null && position != null) {
+      canSetValue = position.inMilliseconds > 0;
+      canSetValue &= position.inMilliseconds < duration.inMilliseconds;
+    }
+
+    double width = widgetWidth - _controlSize - _deleteBtnSize;
+    width -= _deleteBtnSize;
+
+    return SizedBox(
+      width: width,
+      child: Slider(
+        activeColor: Theme.of(context).primaryColor,
+        inactiveColor: Theme.of(context).colorScheme.secondary,
+        onChanged: (v) {
+          if (duration != null) {
+            final position = v * duration.inMilliseconds;
+            _audioPlayer.seek(Duration(milliseconds: position.round()));
+          }
+        },
+        value: canSetValue && duration != null && position != null
+            ? position.inMilliseconds / duration.inMilliseconds
+            : 0.0,
+      ),
+    );
+  }
+
+  Future<void> play() => _audioPlayer.play(_source);
+
+  Future<void> pause() async {
+    await _audioPlayer.pause();
+    setState(() {});
+  }
+
+  Future<void> stop() async {
+    await _audioPlayer.stop();
+    setState(() {});
+  }
+
+  Source get _source =>
+      kIsWeb ? ap.UrlSource(widget.source) : ap.DeviceFileSource(widget.source);
+}
