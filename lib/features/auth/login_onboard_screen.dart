affa462f619a21de2dd418ce78f8deecc322405c
monseigneur0 <82815356+monseigneur0@users.noreply.github.com>
2024-03-02 13:07:20 +0900

    Add login page navigation on skip

--- lib/features/auth/login_onboard_screen.dart
+++ lib/features/auth/login_onboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -72,6 +73,10 @@ class LoginOnboardScreen extends StatelessWidget {
 
       showSkipButton: true,
       skip: const Text('skip'),
+      onSkip: () {
+        // 로그인 페이지로 이동
+        context.go(LoginHome.routeURL);
+      },
 
       // showBackButton = 뒤로가기 버튼 활성화 여부, 첫번째 페이지가 아닐 때 활성화
       showBackButton: false,
