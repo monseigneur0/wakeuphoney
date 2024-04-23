f11b3194b0e9921cdc986597efe6a0fd17f6f829
monseigneur0 <82815356+monseigneur0@users.noreply.github.com>
2024-04-23 13:05:41 +0900

    linux changes generated

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -9,7 +9,6 @@
 #include <audioplayers_linux/audioplayers_linux_plugin.h>
 #include <desktop_webview_auth/desktop_webview_auth_plugin.h>
 #include <file_selector_linux/file_selector_plugin.h>
-#include <isar_flutter_libs/isar_flutter_libs_plugin.h>
 #include <record_linux/record_linux_plugin.h>
 #include <url_launcher_linux/url_launcher_plugin.h>
 
@@ -23,9 +22,6 @@ void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) file_selector_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "FileSelectorPlugin");
   file_selector_plugin_register_with_registrar(file_selector_linux_registrar);
-  g_autoptr(FlPluginRegistrar) isar_flutter_libs_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "IsarFlutterLibsPlugin");
-  isar_flutter_libs_plugin_register_with_registrar(isar_flutter_libs_registrar);
   g_autoptr(FlPluginRegistrar) record_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "RecordLinuxPlugin");
   record_linux_plugin_register_with_registrar(record_linux_registrar);
